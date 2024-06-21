e4d4de5438c27e42c170b8953e449540a587581d
SuperHands <anthon3869@gmail.com>
2024-06-20 23:07:04 -0500

    Update Magnetometer.cpp
    removed definitions, improved output of error handling of read/write bytes.

--- src/Magnetometer.cpp
+++ src/Magnetometer.cpp
@@ -1,39 +1,24 @@
 #include "Magnetometer.h"
 
-#define FXOS8700CQ_ADDRESS 0x1E
-#define FXOS8700CQ_WHO_AM_I 0x0D
-#define FXOS8700CQ_M_DR_STATUS 0x32
-#define FXOS8700CQ_M_OUT_X_MSB 0x33    
-#define FXOS8700CQ_M_OUT_X_LSB 0x34
-#define FXOS8700CQ_M_OUT_Y_MSB 0x35
-#define FXOS8700CQ_M_OUT_Y_LSB 0x36
-#define FXOS8700CQ_M_OUT_Z_MSB 0x37
-#define FXOS8700CQ_M_OUT_Z_LSB 0x38
-#define FXOS8700CQ_M_OFF_X_MSB 0x3F    
-#define FXOS8700CQ_M_OFF_X_LSB 0x40
-#define FXOS8700CQ_M_OFF_Y_MSB 0x41
-#define FXOS8700CQ_M_OFF_Y_LSB 0x42
-#define FXOS8700CQ_M_OFF_Z_MSB 0x43
-#define FXOS8700CQ_M_OFF_Z_LSB 0x44
-#define FXOS8700CQ_M_CTRL_REG1 0x5B
-#define FXOS8700CQ_M_CTRL_REG2 0x5C
-
 Magnetometer::Magnetometer() {}
 
 void Magnetometer::begin(uint32_t i2cRate) {
-  Wire.begin();
-  Wire.setClock(i2cRate);
-  initialize();
-  magOffset();
+    Wire.begin();
+    Wire.setClock(i2cRate);
+    if (initialize()) {
+        magOffset();
+    } else {
+        Serial.println("Magnetometer initialization failed. Skipping calibration.");
+    }
 }
 
 void Magnetometer::update() {
-  if (readByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_DR_STATUS) & 0x08) {
-    readMagData(magCount);
-    mx = (float)magCount[0] * mRes;
-    my = (float)magCount[1] * mRes;   
-    mz = (float)magCount[2] * mRes;
-  }
+    if (readByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_DR_STATUS) & 0x08) {
+        readMagData(magCount);
+        mx = (float)magCount[0] * mRes;
+        my = (float)magCount[1] * mRes;   
+        mz = (float)magCount[2] * mRes;
+    }
 }
 
 float Magnetometer::getX() { return mx; }
@@ -43,113 +28,141 @@ float Magnetometer::getZ() { return mz; }
 void Magnetometer::calibrate() { magOffset(); }
 
 void Magnetometer::readMagData(int16_t *destination) {
-  uint8_t rawData[6];
-  readBytes(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OUT_X_MSB, 6, rawData);
-  destination[0] = (int16_t)(((rawData[0] << 8) | rawData[1]));
-  destination[1] = (int16_t)(((rawData[2] << 8) | rawData[3]));
-  destination[2] = (int16_t)(((rawData[4] << 8) | rawData[5]));
+    uint8_t rawData[6];
+    readBytes(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OUT_X_MSB, 6, &rawData[0]);
+    destination[0] = (int16_t)(((rawData[0] << 8) | rawData[1]));
+    destination[1] = (int16_t)(((rawData[2] << 8) | rawData[3]));
+    destination[2] = (int16_t)(((rawData[4] << 8) | rawData[5]));
 }
 
 void Magnetometer::magOffset() {
-  uint16_t ii = 0, sample_count = 0;
-  int16_t mag_bias[3] = {0, 0, 0};
-  int16_t mag_max[3] = {INT16_MIN, INT16_MIN, INT16_MIN};
-  int16_t mag_min[3] = {INT16_MAX, INT16_MAX, INT16_MAX};
-  int16_t mag_temp[3] = {0, 0, 0};
+    uint16_t ii = 0, sample_count = 0;
+    int16_t mag_bias[3] = {0, 0, 0}, mag_scale[3] = {0, 0, 0};
+    int16_t mag_max[3] = {0x7fff, 0x7fff, 0x7fff}, mag_min[3] = {0x7FFF, 0x7FFF, 0x7FFF}, mag_temp[3] = {0, 0, 0};
+    float dest1[3] = {0, 0, 0}, dest2[3] = {0, 0, 0};
 
-  Serial.println("Mag Calibration: Wave device in a figure eight until done!");
-  delay(4000);
+    Serial.println("Mag Calibration: Wave device in a figure eight until done!");
+    delay(4000);
 
-  standby();
+    standby();
 
-  readMagData(mag_temp);
-  readByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_DR_STATUS);
-
-  writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, magOSR << 2 | 0x03);
-  activate();
-
-  sample_count = 512;
-  for (ii = 0; ii < sample_count; ii++) {
     readMagData(mag_temp);
-    for (int jj = 0; jj < 3; jj++) {
-      if (mag_temp[jj] > mag_max[jj]) mag_max[jj] = mag_temp[jj];
-      if (mag_temp[jj] < mag_min[jj]) mag_min[jj] = mag_temp[jj];
+    readByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_STATUS);
+
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, magOSR << 2 | 0x03);
+    activate();
+
+    sample_count = 512;
+    for (ii = 0; ii < sample_count; ii++) {
+        readMagData(mag_temp);
+        for (int jj = 0; jj < 3; jj++) {
+            if (mag_temp[jj] > mag_max[jj]) mag_max[jj] = mag_temp[jj];
+            if (mag_temp[jj] < mag_min[jj]) mag_min[jj] = mag_temp[jj];
+        }
+        delay(10);
     }
-    delay(10);
-  }
 
-  mag_bias[0] = (mag_max[0] + mag_min[0]) / 2;
-  mag_bias[1] = (mag_max[1] + mag_min[1]) / 2;
-  mag_bias[2] = (mag_max[2] + mag_min[2]) / 2;
+    mag_bias[0] = (mag_max[0] + mag_min[0]) / 2;
+    mag_bias[1] = (mag_max[1] + mag_min[1]) / 2;
+    mag_bias[2] = (mag_max[2] + mag_min[2]) / 2;
 
-  writeMagBias(FXOS8700CQ_ADDRESS, mag_bias);
-  Serial.println("Mag Calibration done!");
+    dest1[0] = (float)mag_bias[0] * mRes;
+    dest1[1] = (float)mag_bias[1] * mRes;   
+    dest1[2] = (float)mag_bias[2] * mRes;
 
-  activate();
+    mag_scale[0] = (mag_max[0] - mag_min[0]) / 2;
+    mag_scale[1] = (mag_max[1] - mag_min[1]) / 2;
+    mag_scale[2] = (mag_max[2] - mag_min[2]) / 2;
+
+    float avg_rad = (mag_scale[0] + mag_scale[1] + mag_scale[2]) / 3.0;
+
+    dest2[0] = avg_rad / ((float)mag_scale[0]);
+    dest2[1] = avg_rad / ((float)mag_scale[1]);
+    dest2[2] = avg_rad / ((float)mag_scale[2]);
+
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OFF_X_MSB, ((mag_bias[0] << 1) & 0xFF00) >> 8);
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OFF_X_LSB, (mag_bias[0] << 1) & 0x00FF);
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OFF_Y_MSB, ((mag_bias[1] << 1) & 0xFF00) >> 8);
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OFF_Y_LSB, (mag_bias[1] << 1) & 0x00FF);
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OFF_Z_MSB, ((mag_bias[2] << 1) & 0xFF00) >> 8);
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_OFF_Z_LSB, (mag_bias[2] << 1) & 0x00FF);
+    Serial.println("Mag Calibration done!");
+
+    activate();
 }
 
 void Magnetometer::standby() {
-  byte c = readByte(FXOS8700CQ_ADDRESS, 0x2A);
-  writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, c & ~(0x01));
+    byte c = readByte(FXOS8700CQ_ADDRESS, 0x2A);
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, c & ~(0x01));
 }
 
 void Magnetometer::activate() {
-  byte c = readByte(FXOS8700CQ_ADDRESS, 0x2A);
-  writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, c | 0x01);
+    byte c = readByte(FXOS8700CQ_ADDRESS, 0x2A);
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, c | 0x01);
 }
 
-void Magnetometer::initialize() {
-  standby();
-  writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, magOSR << 2 | 0x03);
-  activate();
+bool Magnetometer::initialize() {
+    standby();
+    writeByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_M_CTRL_REG1, magOSR << 2 | 0x03);
+    activate();
+    
+    // Check connection
+    return checkConnection();
 }
 
 void Magnetometer::writeByte(uint8_t address, uint8_t subAddress, uint8_t data) {
-  Wire.beginTransmission(address);
-  Wire.write(subAddress);
-  Wire.write(data);
-  uint8_t result = Wire.endTransmission();
-  if (result != 0) {
-    Serial.print("I2C write error: "); Serial.println(result);
-  }
+    Wire.beginTransmission(address);
+    Wire.write(subAddress);
+    Wire.write(data);
+    uint8_t error = Wire.endTransmission();
+    if (error != 0) {
+        Serial.print("I2C write error: ");
+        Serial.println(error);
+    }
 }
 
 uint8_t Magnetometer::readByte(uint8_t address, uint8_t subAddress) {
-  uint8_t data;
-  Wire.beginTransmission(address);
-  Wire.write(subAddress);
-  uint8_t result = Wire.endTransmission(false);
-  if (result != 0) {
-    Serial.print("I2C write error: "); Serial.println(result);
-  }
-  Wire.requestFrom(address, (size_t)1);
-  data = Wire.read();
-  return data;
+    uint8_t data;
+    Wire.beginTransmission(address);
+    Wire.write(subAddress);
+    uint8_t error = Wire.endTransmission(I2C_NOSTOP);
+    if (error != 0) {
+        Serial.print("I2C write error: ");
+        Serial.println(error);
+        return 0; // Return 0 in case of error
+    }
+    Wire.requestFrom(address, (size_t)1);
+    if (Wire.available()) {
+        data = Wire.read();
+    } else {
+        Serial.println("I2C read error: No data available");
+        data = 0; // Return 0 in case of error
+    }
+    return data;
 }
 
 void Magnetometer::readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t *dest) {
-  Wire.beginTransmission(address);
-  Wire.write(subAddress);
-  uint8_t result = Wire.endTransmission(false);
-  if (result != 0) {
-    Serial.print("I2C write error: "); Serial.println(result);
-  }
-  Wire.requestFrom(address, count);
-  for (uint8_t i = 0; i < count; i++) {
-    dest[i] = Wire.read();
-  }
-}
-
-void Magnetometer::writeMagBias(uint8_t address, int16_t *bias) {
-  writeByte(address, FXOS8700CQ_M_OFF_X_MSB, ((bias[0] << 1) & 0xFF00) >> 8);
-  writeByte(address, FXOS8700CQ_M_OFF_X_LSB, (bias[0] << 1) & 0x00FF);
-  writeByte(address, FXOS8700CQ_M_OFF_Y_MSB, ((bias[1] << 1) & 0xFF00) >> 8);
-  writeByte(address, FXOS8700CQ_M_OFF_Y_LSB, (bias[1] << 1) & 0x00FF);
-  writeByte(address, FXOS8700CQ_M_OFF_Z_MSB, ((bias[2] << 1) & 0xFF00) >> 8);
-  writeByte(address, FXOS8700CQ_M_OFF_Z_LSB, (bias[2] << 1) & 0x00FF);
+    Wire.beginTransmission(address);
+    Wire.write(subAddress);
+    uint8_t error = Wire.endTransmission(I2C_NOSTOP);
+    if (error != 0) {
+        Serial.print("I2C write error: ");
+        Serial.println(error);
+        return; // Return in case of error
+    }
+    Wire.requestFrom(address, (size_t)count);
+    if (Wire.available() == count) {
+        for (uint8_t i = 0; i < count; i++) {
+            dest[i] = Wire.read();
+        }
+    } else {
+        Serial.println("I2C read error: Incomplete data");
+    }
 }
 
 bool Magnetometer::checkConnection() {
     uint8_t whoAmI = readByte(FXOS8700CQ_ADDRESS, FXOS8700CQ_WHO_AM_I);
+    Serial.print("WHO_AM_I register: 0x");
+    Serial.println(whoAmI, HEX);
     return (whoAmI == 0xC7);
 }
